fb6ff5d769f04f6335c6c277efcb6c2027478a51
Josh Lin <jlin9695@gmail.com>
2022-12-05 18:14:46 -0600

    Filled out most of the methods, and started adding/calling them in the script file.

--- scriptHelper.js
+++ scriptHelper.js
@@ -14,26 +14,61 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
                 </ol>
                 <img src="">
    */
+  
 }
 
 function validateInput(testInput) {
-   
+   if((testInput.value) === ''){
+    return "Empty"
+   }
+   if(typeof(testInput.value) === 'number'){
+    return "Is a Number"
+   }
+   else{
+    return "Not a number"
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    const status = document.getElementById("launchStatus")
+    const div = document.getElementById("faultyItems")
+    const pilotElement = document.getElementById("pilotStatus");
+    const copilotElement = document.getElementById("copilotStatus");
+    const fuel = document.getElementById("fuelStatus");
+    const cargo = document.getElementById("cargoStatus")
+    pilotElement.innerHTML = `Pilot ${pilot}.`;
+    copilotElement.innerHTML = `CoPilot ${copilot}.`
+    if(fuelLevel < 10000){
+        div.style.visibility = "visible"
+        fuel.innerHTML = "Not enough fuel for the journey."
+        status.style.color = "red"
+        status.innerHTML = "Shuttle not ready for launch."
+    }
+    if(cargoLevel > 10000){
+        div.style.visibility = "visible"
+        cargo.innerHTML = "Too much cargo to take off."
+        status.style.color = "red"
+        status.innerHTML = "Shuttle not ready for launch."
+    }
+    else{
+        status.style.color = "green"
+        status.innerHTML = "Shuttle is ready for launch!"
+    }
    
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        response.json()
         });
-
+    console.log(planetsReturned)
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    return planets[Math.floor(Math.random() * (6))]
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
